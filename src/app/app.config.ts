30208cd8dd9b0812416f814f1ac82224d76d14f6
kevin cuadros <kevindavidcuadros@gmail.com>
2026-03-18 18:58:07 -0500

    Add PWA support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, isDevMode, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import { AuthStore } from './core/auth.store';
@@ -9,5 +10,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAppInitializer(() => { inject(AuthStore); }),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ]
 };
