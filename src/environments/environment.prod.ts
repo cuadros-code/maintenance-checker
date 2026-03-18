e012dafb0297591cd70b85ddaadfddb31ffca60b
kevin cuadros <kevindavidcuadros@gmail.com>
2026-03-18 18:44:45 -0500

    add enviroment data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: '',
-  supabaseKey: '',
+  supabaseUrl: import.meta.env.NG_APP_SP_URL,
+  supabaseKey: import.meta.env.NG_APP_SP_KEY,
 };
